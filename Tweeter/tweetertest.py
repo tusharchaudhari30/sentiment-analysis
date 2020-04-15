74ee4c0507215c54d501e683382deaad164a4c57
tusharchaudhari30 <blazeric30@gmail.com>
2020-04-15 17:52:23 +0530

    Update tweetertest.py

--- Tweeter/tweetertest.py
+++ Tweeter/tweetertest.py
@@ -1,7 +1,7 @@
 import tweepy
 
-auth = tweepy.OAuthHandler('awhd0FTt5WJOY1NfckYAvqseU', 'p4mFuUK9RpfgLMZ8HYj9EnMS99oJ2WmWQpY1rkbdyIPLzIdZm1')
-auth.set_access_token('1215597718413836288-0cXv5KvZJuqM8SiHKWfNobjRmvSIjb', 'MISbW379sRaCN9hjQNpUpfgKmSifzy0zh3qyarjdvGS37')
+auth = tweepy.OAuthHandler('', ')
+auth.set_access_token('', '')
 
 api = tweepy.API(auth)
 
